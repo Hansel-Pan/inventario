78bc2fb88ce4f628c31118d1e82984fd794fa609
Hpan <Hpan3122@gmail.com>
2025-11-15 18:37:23 -0500

    Connecting DB - HTTP:Get

--- src/controllers/categorias.controllers.js
+++ src/controllers/categorias.controllers.js
@@ -1,10 +1,19 @@
-const getCategorias = (req, res) => {
+import getConnection from "../db/database.js";
 
-    res.json({"Categoria":"Electrodomesticos"})
+const getCategorias = async (req, res) => {
+    
+  try {
+    const connection = await getConnection();
+    const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen from categorias");
+    res.json(result);
+    }
+    catch(error){
+        console.error("ERROR 500");
+        
+    }
 
-} 
+};
 
 export const methodHTTP = {
-
-    getCategorias
-}
+  getCategorias,
+};
