ad7e7d193c3532f2c58f046964963553cfa4b0e8
Hpan <Hpan3122@gmail.com>
2025-11-15 19:29:53 -0500

    Adding Frontend and API Connection

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express"
 import categoriasRoutes from "./routes/categorias.routes.js"
+import cors from "cors"
 
 // Asignamos a app toda la funcionalidad 
+
 const app = express()
+
+app.use(cors())
 
 app.set("port", 5000);
 
+
 /* Routes */
 app.use("/api/categorias", categoriasRoutes)
+
 
 export default app
